30dd83d9a8f30401d40ba035a2e23a1277d41795
Nikolay Beketov <nbek@miko.ru>
2024-04-03 06:17:21 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleZabbixAgent5
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulezabbixagent5/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -17,10 +17,10 @@ return [
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
-    'repModuleZabbixAgent5' => '',
-    'mo_ModuleModuleZabbixAgent5' => '',
-    'BreadcrumbModuleZabbixAgent5' => '',
-    'SubHeaderModuleZabbixAgent5' => '',
-    'modzbx_ConfigFileLabel' => '',
-    'fw_modulezabbixagent5Description' => '',
+    'repModuleZabbixAgent5' => 'Zabbix agent 5- %repesent%',
+    'mo_ModuleModuleZabbixAgent5' => 'Agent Zabbix 5',
+    'BreadcrumbModuleZabbixAgent5' => 'Agent Zabbix 5',
+    'SubHeaderModuleZabbixAgent5' => 'Sledování MikoPBX přes Zabbix',
+    'modzbx_ConfigFileLabel' => 'Konfigurační soubor agenta',
+    'fw_modulezabbixagent5Description' => 'Zabbix - sledování MikoPBX přes Zabbix',
 ];
