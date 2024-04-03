5fdc0a19a6415cedb4079e7ee0a65062fdd87e79
Nikolay Beketov <nbek@miko.ru>
2024-04-03 06:22:04 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleZabbixAgent5
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulezabbixagent5/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -17,10 +17,10 @@ return [
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
-    'repModuleZabbixAgent5' => '',
-    'mo_ModuleModuleZabbixAgent5' => '',
-    'BreadcrumbModuleZabbixAgent5' => '',
-    'SubHeaderModuleZabbixAgent5' => '',
-    'modzbx_ConfigFileLabel' => '',
-    'fw_modulezabbixagent5Description' => '',
+    'repModuleZabbixAgent5' => 'Zabbix-agent 5-%repesent%',
+    'mo_ModuleModuleZabbixAgent5' => 'Zabbix-agent 5',
+    'BreadcrumbModuleZabbixAgent5' => 'Zabbix-agent 5',
+    'SubHeaderModuleZabbixAgent5' => 'MikoPBX monitoren via Zabbix',
+    'modzbx_ConfigFileLabel' => 'Agentconfiguratiebestand',
+    'fw_modulezabbixagent5Description' => 'Zabbix - MikoPBX monitoren via Zabbix',
 ];
