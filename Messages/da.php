1c60c3c18140f39494a2ee553115660e928359ea
Nikolay Beketov <nbek@miko.ru>
2024-04-03 06:21:17 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleZabbixAgent5
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulezabbixagent5/da/

--- Messages/da.php
+++ Messages/da.php
@@ -17,7 +17,7 @@ return [
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
-    'repModuleZabbixAgent5' => '',
+    'repModuleZabbixAgent5' => 'Zabbix agent 5- %repesent%',
     'mo_ModuleModuleZabbixAgent5' => 'Zabbix agent 5',
     'BreadcrumbModuleZabbixAgent5' => 'Zabbix agent 5',
     'SubHeaderModuleZabbixAgent5' => 'Overvågning af MikoPBX via Zabbix',
